849725ffb19a4d23cddb276aec4b691be7b9386b
Thaisa Alice <thaisalic3@icloud.com>
2023-06-03 17:41:08 -0300

    feat: add initial config and command ping

---
+++ deploy_commands.js
@@ -0,0 +1,34 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const commands = [];
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} (/) commands...`);
+		const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+		await rest.put(
+			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log('Commands registered successfully! 🔥');
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN } = process.env;
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[ERROR] Command ${filePath} is not a valid command.`);
+	}
+}
+
+client.once(Events.ClientReady, c => {
+	console.log(`🤖Ready! Logged in as ${c.user.tag}🔥`);
+});
+client.login(TOKEN);
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) console.error('[ERROR] Command is not a valid command.');
+	command.execute(interaction);
+});
